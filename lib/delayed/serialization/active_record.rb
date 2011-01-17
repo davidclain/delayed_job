54d134933b1fc6476669aea0e64430dd1c8bfc25
David Clain <david@camelotapp.com>
2011-01-17 15:42:32 -0500

    Still trying.

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -2,6 +2,14 @@ class ActiveRecord::Base
   yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
 
   def self.yaml_new(klass, tag, val)
+    klass.establish_connection(
+      :adapter => 'mysql',
+  	  :host => 'localhost',
+  	  :database => 'new_dev',
+  	  :username => 'root',
+  	  :password => ''
+    )
+
     klass.find(val['attributes']['id'])
   rescue ActiveRecord::RecordNotFound
     raise Delayed::DeserializationError
