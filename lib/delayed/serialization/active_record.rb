cb45ecf9f690ab94ed8135d0b62316c78f289b81
David Clain <david@camelotapp.com>
2011-01-17 15:53:05 -0500

    Another attempt

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -2,7 +2,7 @@ class ActiveRecord::Base
   yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
 
   def self.yaml_new(klass, tag, val)
-    Import.establish_connection(
+    ::ActiveRecord::Base.establish_connection(
       :adapter => 'mysql',
   	  :host => 'localhost',
   	  :database => 'new_dev',
