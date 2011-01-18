93e0669689d2f4f42372e45bb48e6e641b62064d
David Clain <david@camelotapp.com>
2011-01-17 22:02:40 -0500

    Trying to get this to choose database dynamically

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -2,7 +2,7 @@ class ActiveRecord::Base
   yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
 
   def self.yaml_new(klass, tag, val)
-    account = Account.find_by_subdomain('dev')
+    account = Account.find_by_subdomain(val['attributes']['account_subdomain'])
     ::ActiveRecord::Base.establish_connection(
 		  :adapter	=> 'mysql',
 		  :host			=> account.database.host,
