4387d30a281a33c8a4168b4828b74ed202550d2d
David Clain <david@camelotapp.com>
2011-11-03 23:55:44 -0400

    If we can't get an account, don't try to hijack the database or websolr.
    This is useful for cases in which we can't automatically do the hijacking but the DJ object can do it itself

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -3,17 +3,20 @@ class ActiveRecord::Base
 
   def self.yaml_new(klass, tag, val)
     account = Account.find_by_subdomain(val['attributes']['account_subdomain'])
-    ::ActiveRecord::Base.establish_connection(
-		  :adapter	=> 'mysql',
-		  :host			=> account.database.host,
-		  :port			=> account.database.port,
-		  :database	=> account.database.name,
-		  :username	=> account.database.username,
-		  :password	=> account.database.password
-    )
 
-    # Use the right Websolr URL
-    Sunspot.config.solr.url = account.websolr_url
+    if account
+      ::ActiveRecord::Base.establish_connection(
+		    :adapter	=> 'mysql',
+		    :host			=> account.database.host,
+		    :port			=> account.database.port,
+		    :database	=> account.database.name,
+		    :username	=> account.database.username,
+		    :password	=> account.database.password
+      )
+
+      # Use the right Websolr URL
+      Sunspot.config.solr.url = account.websolr_url
+    end
 
     klass.find(val['attributes']['id'])
   rescue ActiveRecord::RecordNotFound
